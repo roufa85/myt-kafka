035f0a8ff35d9910439d014bfab07e4cc3cc2298
elkousy <walid.elkousy@gmail.com>
2020-01-24 07:27:10 +0100

    remove misc files

--- misc/kafka-cluster-setup/setup/setup-12-kafka-topics-ui.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# copy the kafka-topics-ui-docker-compose.yml file
-nano kafka-topics-ui-docker-compose.yml
-
-# launch it
-docker-compose -f  kafka-topics-ui-docker-compose.yml up -d
--- misc/kafka-cluster-setup/setup/setup-4-zoonavigator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nano zoonavigator-docker-compose.yml
-# Make sure port 8001 is opened on the instance security group
-
-# copy the zookeeper/zoonavigator-docker-compose.yml file
-# run it
-docker-compose -f zoonavigator-docker-compose.yml up -d
